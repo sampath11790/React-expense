cf2672cd404781b3a70e4d18dec04ae39e0a94d1
sampath11790 <bsampath11790@gmail.com>
2022-09-26 18:31:45 +0530

    expense

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ExpenceItem from "./components/ExpenseItem";
+import Expence from "./components/Item";
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenceItem></ExpenceItem>
+      <Expence></Expence>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,5 @@
+function ExpenceItem(){
+    return <h2>This is expense Item</h2>
+}
+export default ExpenceItem
+
