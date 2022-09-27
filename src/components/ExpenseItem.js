fb4d47f5ab871a5ee655cf0f84bf435ccc5de219
sampath11790 <bsampath11790@gmail.com>
2022-09-27 17:39:38 +0530

    used variable and add location

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,22 @@
+
+import './ExpenseItem.css'
 function ExpenceItem(){
+   const ExpenseDate=new Date(2022,2,22);
+   const ExpenseTitle="Car Insurance";
+   const ExpenseAmount=294.67;
+    const LocationOfExpenditure="Bangalore";
+
     return (
-    <div>
-        <h2>This is expense Item</h2>,
-            <h1>hi</h1>
-        </div>)
+    <div className="expense-item">
+       <div>{ExpenseDate.toISOString()}</div>
+       <div className='expense-item__description'>
+        <h2>{ExpenseTitle}</h2>
+       <div className='Location-Expenditure'>
+       <h2>{LocationOfExpenditure}</h2>
+       </div>
+     <div className='expense-item__price'>{"$"+ExpenseAmount}</div>
+       </div>
+     </div>);
 }
 export default ExpenceItem
 
